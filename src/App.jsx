3c343abf806bad8a3a871c23e8adf78e5080f6be
Alankar Singh <alankar010499@gmail.com>
2024-05-07 13:16:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Body from "./components/Body.jsx";
+
+function App() {
+  return (
+    <div>
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header.jsx";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className = "absolute" >
+        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/d253acf4-a1e2-4462-a416-f78802dc2d85/f04bf88c-f71c-4d02-82ed-adb870b8f8db/IN-en-20240429-POP_SIGNUP_TWO_WEEKS-perspective_WEB_658a042e-62cf-473d-8da0-7b875f23e2ef_large.jpg"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
