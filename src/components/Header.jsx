9223411e56514b74803786b3e47ee6aa036d3fa6
Alankar Singh <alankar010499@gmail.com>
2024-05-07 17:42:46 +0530

    loginForm

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="">
       <img
-        className="w-44 absolute z-40 bg-gradient-to-b from-black"
+        className="w-44 z-40 absolute bg-gradient-to-b from-black "
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
       />
     </div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header.jsx";
 
 const Login = () => {
+  const [isSignUpForm, setIsSignUpForm] = useState(false);
+  const handleSignUpForm = () => {
+    setIsSignUpForm(!isSignUpForm);
+  };
   return (
-    <div>
+    <div className="">
       <Header />
-      <div className = "absolute" >
+      <div className="absolute">
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/d253acf4-a1e2-4462-a416-f78802dc2d85/f04bf88c-f71c-4d02-82ed-adb870b8f8db/IN-en-20240429-POP_SIGNUP_TWO_WEEKS-perspective_WEB_658a042e-62cf-473d-8da0-7b875f23e2ef_large.jpg"></img>
       </div>
+      <form className="absolute w-3/12 bg-black m-auto left-0 right-0 p-12 my-36  text-white bg-opacity-80 rounded-lg ">
+        <h1 className="text-3xl p-2 my-2 font-bold">
+          {isSignUpForm ? "Sign Up " : "Sign In"}
+        </h1>
+        {isSignUpForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 bg-neutral-900 w-full bg-opacity-70 "
+          ></input>
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 bg-neutral-900 w-full bg-opacity-70 "
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 bg-neutral-900 w-full bg-opacity-70"
+        ></input>
+        <button type="submit" className="p-2 my-2 bg-red-600 w-full rounded-sm">
+          Sign In
+        </button>
+        <p className="py-5 font-semibold cursor-pointer" onClick={handleSignUpForm}>
+          {isSignUpForm
+            ? "Already Signed up? Sign in now."
+            : "New to Netflix? Sign up now."}
+        </p>
+      </form>
     </div>
   );
 };
