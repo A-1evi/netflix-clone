8c15685f8d5f616a74fda997df570c5caeb9054d
Alankar Singh <alankar010499@gmail.com>
2024-05-13 12:59:31 +0530

    hidding key

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 // eslint-disable-next-line no-undef
-const genAI = new GoogleGenerativeAI(import.meta.env.REACT_APP_API_KEY);
+const genAI = new GoogleGenerativeAI(import.meta.env.API_KEY);
 
 // ...
 
